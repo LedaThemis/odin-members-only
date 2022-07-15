2e8996c5e92ccf3817ba359bee085385029c99e9
Leda <ledathemis@protonmail.com>
2022-07-15 16:07:23 +0000

    implement join club_post

--- controllers/joinController.js
+++ controllers/joinController.js
@@ -1,9 +1,35 @@
+const User = require('../models/user');
+
 exports.join_club_get = function (req, res, next) {
   res.render('index', { title: 'Join Members Club', content: 'join/club', props: { errors: undefined } });
 };
 
 exports.join_club_post = function (req, res, next) {
-  res.send('NOT IMPLEMENTED');
+  if (req.user) {
+    if (req.user.isMember) {
+      res.render('index', {
+        title: 'Join Members Club',
+        content: 'join/club',
+        props: { errors: [{ msg: "You're already a member!" }] },
+      });
+    } else if (req.body.passcode === process.env.CLUB_PASSCODE) {
+      User.findByIdAndUpdate(req.user.id, { isMember: true }, {}, (err, updatedUser) => {
+        if (err) {
+          return next(err);
+        }
+
+        res.redirect('/');
+      });
+    } else {
+      res.render('index', {
+        title: 'Join Members Club',
+        content: 'join/club',
+        props: { errors: [{ msg: 'Wrong passcode! look carefully!' }] },
+      });
+    }
+  } else {
+    res.redirect('/join/club');
+  }
 };
 
 exports.join_admin_get = function (req, res, next) {
