ad511f13835faaec27be623d2f8642b5ce7e9738
Leda <ledathemis@protonmail.com>
2022-07-15 13:36:23 +0000

    implement user register_get

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,5 @@
 exports.register_get = function (req, res, next) {
-  res.send('NOT IMPLEMENTED');
+  res.render('index', { title: 'Register', content: 'user/register', props: { user: undefined, errors: undefined } });
 };
 
 exports.register_post = function (req, res, next) {
@@ -11,20 +11,5 @@ exports.login_get = function (req, res, next) {
 };
 
 exports.login_post = function (req, res, next) {
-  res.send('NOT IMPLEMENTED');
-};
-
-exports.join_club_get = function (req, res, next) {
-  res.send('NOT IMPLEMENTED');
-};
-exports.join_club_post = function (req, res, next) {
-  res.send('NOT IMPLEMENTED');
-};
-
-exports.join_admin_get = function (req, res, next) {
-  res.send('NOT IMPLEMENTED');
-};
-
-exports.join_admin_post = function (req, res, next) {
   res.send('NOT IMPLEMENTED');
 };
