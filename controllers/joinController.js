117aa454f304a7be42d18b4228c1c4f85b70e82b
Leda <ledathemis@protonmail.com>
2022-07-15 16:15:14 +0000

    implement join admin_post

--- controllers/joinController.js
+++ controllers/joinController.js
@@ -37,5 +37,29 @@ exports.join_admin_get = function (req, res, next) {
 };
 
 exports.join_admin_post = function (req, res, next) {
-  res.send('NOT IMPLEMENTED');
+  if (req.user) {
+    if (req.user.isAdmin) {
+      res.render('index', {
+        title: 'Become Admin',
+        content: 'join/admins',
+        props: { errors: [{ msg: "You're already an admin!" }] },
+      });
+    } else if (req.body.passcode === process.env.ADMINS_PASSCODE) {
+      User.findByIdAndUpdate(req.user.id, { isAdmin: true }, {}, (err, updatedUser) => {
+        if (err) {
+          return next(err);
+        }
+
+        res.redirect('/');
+      });
+    } else {
+      res.render('index', {
+        title: 'Become Admin',
+        content: 'join/admins',
+        props: { errors: [{ msg: 'Wrong passcode! look carefully!' }] },
+      });
+    }
+  } else {
+    res.redirect('/join/admins');
+  }
 };
