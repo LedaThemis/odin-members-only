079aa32ca5ba06252421b1e0fa0ecfd9d12c2323
Leda <ledathemis@protonmail.com>
2022-07-15 16:38:29 +0000

    implement create_message_get

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,5 +1,9 @@
 exports.create_message_get = function (req, res, next) {
-  res.send('NOT IMPLEMENTED');
+  res.render('index', {
+    title: 'Create Message',
+    content: 'message/create',
+    props: { message: undefined, errors: undefined },
+  });
 };
 
 exports.create_message_post = function (req, res, next) {
