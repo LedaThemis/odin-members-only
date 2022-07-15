8885ffed11bbe6805f98ac731a572f31fb46562c
Leda <ledathemis@protonmail.com>
2022-07-15 17:09:53 +0000

    move home controller; load messages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+const homeController = require('../controllers/homeController');
+
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Members only', content: 'home', props: {} });
-});
+router.get('/', homeController.index);
 
 module.exports = router;
