8accec1227c85ee75a4c6eb9e0eb68bdddf18ee1
Leda <ledathemis@protonmail.com>
2022-07-15 16:06:13 +0000

    implement join club_get

--- controllers/joinController.js
+++ controllers/joinController.js
@@ -1,5 +1,5 @@
 exports.join_club_get = function (req, res, next) {
-  res.send('NOT IMPLEMENTED');
+  res.render('index', { title: 'Join Members Club', content: 'join/club', props: { errors: undefined } });
 };
 
 exports.join_club_post = function (req, res, next) {
