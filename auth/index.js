6eddb647140a2bb1b3e373f5b3eec4bba3aaa50d
Leda <ledathemis@protonmail.com>
2022-07-15 15:23:27 +0000

    fix auth strategy

--- auth/index.js
+++ auth/index.js
@@ -8,8 +8,12 @@ const User = require('../models/user');
  */
 exports.strategy = new LocalStrategy((username, password, done) => {
   User.findOne({ username: username }, (err, user) => {
-    if (err) done(err);
-    if (!user) done(null, false, { message: 'Incorrect username' });
+    if (err) {
+      return done(err);
+    }
+    if (!user) {
+      return done(null, false, { message: 'Incorrect username' });
+    }
 
     bcrypt.compare(password, user.password, (err, res) => {
       if (res) {
